8dbbefbc3c230f77be513d278e62ad313cd97d58
MohamadDimasPrayoga1010 <mohamaddimasprayoga89@gmail.com>
2025-09-08 10:00:38 +0700

    Update minitask1.js

--- minitask1.js
+++ minitask1.js
@@ -1,7 +1,8 @@
 let items = ["Apel","Anggur", "Pisang", "Rambutan", "Pir"];
 
 const buah = items.filter(function(buahA){
-    return buahA[0] === "a"
+    return buahA[0] === "A"
 })
+
 
 console.log(buah)
