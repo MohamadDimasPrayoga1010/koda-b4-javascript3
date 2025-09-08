21542e257bdfbc4a885ad79a8a6b29b80f9c1a9a
MohamadDimasPrayoga1010 <mohamaddimasprayoga89@gmail.com>
2025-09-08 15:01:15 +0700

    Add files via upload

---
+++ minitask3.js
@@ -0,0 +1,56 @@
+//then-catch
+
+const wait = (str, time) => {
+  return new Promise((resolve, reject) => {
+    if (typeof str !== "string") {
+      reject("err: harus string");
+    } else {
+      setTimeout(() => {
+        resolve(str);
+      }, time);
+    }
+  });
+};
+
+wait("John", 1500)
+  .then((res) => {
+    console.log(res);
+    wait("Ed", 2000)
+      .then((res) => {
+        console.log(res);
+        wait("Jane", 500)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+async-await
+
+async function antrian() {
+    try {
+      const antrian1 = await wait("John", 1500);
+      console.log(antrian1);
+      const antrian2 = await wait(0, 2000);
+      console.log(antrian2);
+      const antrian3 = await wait("Jane", 500);
+      console.log(antrian3);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+
+antrian()
+
+
